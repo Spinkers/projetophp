db14abb66cf43075239c4d7b7caa521eae778bad
Spinkers <lucasmouraolopes@gmail.com>
2019-11-10 18:26:59 -0300

    Refatorando postagem_create

--- src/php_actions/postagem/create.php
+++ src/php_actions/postagem/create.php
@@ -8,10 +8,10 @@ if(isset($_POST['btn-salvar'])){
     $titulo = mysqli_escape_string($connect, $_POST['titulo']);
     $conteudo = mysqli_escape_string($connect, $_POST['conteudo']);
     $categoria = mysqli_escape_string($connect, $_POST['categoria']);
+    $dataPostagem = '2019-11-10 00:00:00';
     $idUser = $_SESSION['idUser'];
-    $dataPostagem = '2019-08-15';
 
-    $sql = "INSERT INTO Postagem (idUser, idCategoria, título, conteúdo, dataPostagem) VALUES ('$idUser', '$categoria', '$titulo', '$conteudo', '$dataPostagem')";
+    $sql = "INSERT INTO `Postagem` (`conteudo`, `titulo`, `dataPostagem`, `idUser`, `idCategoria`) VALUES ('$conteudo', '$titulo', '$dataPostagem', '$idUser', '$categoria');";
 
     if(mysqli_query($connect, $sql)){
         $_SESSION['mensagem'] = "Cadastrado com sucesso!";
