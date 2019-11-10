365dfc22aabfc6354b29353b54c04652f18af2ad
Spinkers <lucasmouraolopes@gmail.com>
2019-11-10 18:26:19 -0300

    Alterando banco

--- config/conexao.php
+++ config/conexao.php
@@ -1,9 +1,15 @@
 <?php
 //Conexão com banco de dados
-$servername = "remotemysql.com";
-$username = "ApSTt7HJYU";
-$password = "7pOAV5kTXG";
-$db_name = "ApSTt7HJYU";
+
+$servername = "localhost";
+$username = "userTest";
+$password = "#Teste34524519";
+$db_name = "howToLearnPhp";
+
+/*$servername = "mysql:host=localhost:3306";
+$username = "root";
+$password = "root";
+$db_name = "teste";*/
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
 mysqli_set_charset($connect, "utf8");
