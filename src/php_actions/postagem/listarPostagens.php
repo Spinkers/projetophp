00817784d693dd283585ea3e5ff4c268518b714e
Spinkers <lucasmouraolopes@gmail.com>
2019-11-10 18:27:37 -0300

    Refatorando postagem_listar

--- src/php_actions/postagem/listarPostagens.php
+++ src/php_actions/postagem/listarPostagens.php
@@ -4,7 +4,7 @@
 //Conexão
 require_once '../../../config/conexao.php';
 
-$sql = "SELECT Postagem.título, Postagem.dataPostagem, Postagem.conteúdo, Usuario.nome, Categoria.tipo FROM Postagem INNER JOIN Usuario ON Postagem.idUser = Usuario.idUser INNER JOIN Categoria ON Postagem.idCategoria = Categoria.idCategoria ";
+$sql = "SELECT Postagem.idPost, Postagem.titulo, Postagem.dataPostagem, Postagem.conteudo, Usuario.nome, Categoria.tipo FROM Postagem INNER JOIN Usuario ON Postagem.idUser = Usuario.idUser INNER JOIN Categoria ON Postagem.idCategoria = Categoria.idCategoria WHERE Postagem.aprovado = '0'";
 $resultado = mysqli_query($connect, $sql);
 
 ?>
