6762fd1a061a14ffde0d0e5b8010bb020ad00682
Spinkers <lucasmouraolopes@gmail.com>
2019-11-17 15:46:08 -0300

    Renderizando categorias na sidebar da home

--- src/views/includes/sidebar.php
+++ src/views/includes/sidebar.php
@@ -1,94 +1,62 @@
 <!-- Sidebar Holder -->
 <nav id="sidebar">
-            <div class="sidebar-header">
-                <h3>How to Learn</h3>
-            </div>
+    <div class="sidebar-header">
+        <h3>How to Learn</h3>
+    </div>
 
-            <ul class="list-unstyled components">
-                <li class="active">
-                    <a href="#">Início</a>
-                </li>
+    <ul class="list-unstyled components">
+        <li class="active">
+            <a href="#">Início</a>
+        </li>
 
-                <li>
-                    <a href="#basicoSubMenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Básico</a>
-                    <ul class="collapse list-unstyled" id="basicoSubMenu">
-                        <li>
-                            <a href="#">Sintaxe Básica</a>
-                        </li>
-                        <li>
-                            <a href="#">Tipos</a>
-                        </li>
-                        <li>
-                            <a href="#">Variáveis</a>
-                        </li>
-                        <li>
-                            <a href="#">Constantes</a>
-                        </li>
-                        <li>
-                            <a href="#">Expressões</a>
-                        </li>
-                        <li>
-                            <a href="#">Operadores</a>
-                        </li>
-                        <li>
-                            <a href="#">Estruturas de Controle</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#medioSubMenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Intermediário</a>
-                    <ul class="collapse list-unstyled" id="medioSubMenu">
-                        <li>
-                            <a href="#">Funções</a>
-                        </li>
-                        <li>
-                            <a href="#">Classes e Objetos</a>
-                        </li>
-                        <li>
-                            <a href="#">Namescapes</a>
-                        </li>
-                        <li>
-                            <a href="#">Erros</a>
-                        </li>
-                        <li>
-                            <a href="#">Exceções</a>
-                        </li>
-                        <li>
-                            <a href="#">Generators</a>
-                        </li>
-                        <li>
-                            <a href="#">Referências</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#avancadoSubMenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Avançado</a>
-                    <ul class="collapse list-unstyled" id="avancadoSubMenu">
-                        <li>
-                            <a href="#">Variáveis pré-definidas</a>
-                        </li>
-                        <li>
-                            <a href="#">Exceções pré-definidas</a>
-                        </li>
-                        <li>
-                            <a href="#">Interfaces e Classes pré-definidas</a>
-                        </li>
-                        <li>
-                            <a href="#">Opções e parâmetros de contexto</a>
-                        </li>
-                        <li>
-                            <a href="#">Protocolos e Wrappers suportados</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+        <?php
+            define('BASE_URL', getcwd());
+            require_once BASE_URL .'/src/php_actions/categoria/select.php';
 
-            <ul class="list-unstyled CTAs">
-                <li>
-                    <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Teste seu conhecimento</a>
-                </li>
-                <li>
-                    <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Envie sua sugestão</a>
-                </li>
-            </ul>
-        </nav>
+            if (mysqli_num_rows($resultado) > 0) {
+                while ($dados = mysqli_fetch_array($resultado)) {
+                    echo "<li>";
+                    echo "<a href='#$dados[tipo]SubMenu' data-toggle='collapse' aria-expanded='false' class='dropdown-toggle'>$dados[tipo]</a>";
+                    echo "<ul class='collapse list-unstyled' id='$dados[tipo]SubMenu'>
+                    <li>
+                        <a href='#'>Sintaxe Básica</a>
+                    </li>
+                    <li>
+                        <a href='#'>Tipos</a>
+                    </li>
+                    <li>
+                        <a href='#'>Variáveis</a>
+                    </li>
+                    <li>
+                        <a href='#'>Constantes</a>
+                    </li>
+                    <li>
+                        <a href='#'>Expressões</a>
+                    </li>
+                    <li>
+                        <a href='#'>Operadores</a>
+                    </li>
+                    <li>
+                        <a href='#'>Estruturas de Controle</a>
+                    </li>
+                </ul>";
+                    echo "</li>";
+                }
+
+            } else {
+                echo "Nenhuma categoria";
+            }
+        ?>
+
+        </li>
+    </ul>
+
+    <ul class="list-unstyled CTAs">
+        <li>
+            <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Teste seu conhecimento</a>
+        </li>
+        <li>
+            <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Envie sua sugestão</a>
+        </li>
+    </ul>
+</nav>
